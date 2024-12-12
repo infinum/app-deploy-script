0349ce39d11994f0469e4433e624cb6c488f5a5a
Jasmin Abou Aldan <jasmin.aboualdan@infinum.com>
2024-12-12 15:22:43 +0100

    Change

--- sources/__build_tagging.sh
+++ sources/__build_tagging.sh
@@ -15,11 +15,11 @@ function __build_tagging {
     APP_VERSION=""
     APP_PLATFORM=$(__check_platform)
     if [ "$APP_PLATFORM" == "$PLATFORM_ANDROID_APK" ]; then
-        APP_VERSION=$(__generate_app_version_from_apk "$APP_PATH" "$BUILD_VERSION")
+        APP_VERSION=$(__generate_app_version_from_apk "$APP_PATH" "$BUILD_COUNT")
     elif [ "$APP_PLATFORM" == "$PLATFORM_ANDROID_AAB" ]; then
-        APP_VERSION=$(__generate_app_version_from_aab "$APP_PATH" "$BUILD_VERSION")
+        APP_VERSION=$(__generate_app_version_from_aab "$APP_PATH" "$BUILD_COUNT")
     elif [ "$APP_PLATFORM" == "$PLATFORM_IOS" ]; then
-        APP_VERSION=$(__generate_app_version_from_ipa "$APP_PATH" "$BUILD_VERSION")
+        APP_VERSION=$(__generate_app_version_from_ipa "$APP_PATH" "$BUILD_COUNT")
     else
         echo
         echo "Unsupported file format: ${APP_PATH##*.}"
@@ -28,8 +28,20 @@ function __build_tagging {
         exit 1
     fi
 
+    __create_and_push_tag
+}
+
+# Tag creation & push
+
+function __create_and_push_tag {
+
+    # If there is a custom app version passed, append build number to it
+    if [[ -n "$CUSTOM_APP_VERSION" ]]; then
+        CUSTOM_APP_VERSION="${CUSTOM_APP_VERSION}b${BUILD_COUNT}"
+    fi
+
     CHANGELOG=${CHANGELOG:-""} # Set empty string if changelog is not available
-    TAG="${ENVIRONMENT}/v${APP_VERSION}"
+    TAG="${ENVIRONMENT}/v${CUSTOM_APP_VERSION:-$APP_VERSION}"
     git tag -a "$TAG" -m "${CHANGELOG}"
     git push origin "$TAG"
 }
@@ -39,21 +51,22 @@ function __build_tagging {
 function __validate_options {
     shift
 
-    while getopts "e:p:v:c:" opt; do
+    while getopts "e:p:b:v:c:" opt; do
         case "$opt" in
             e) ENVIRONMENT="$OPTARG" ;;
             p) APP_PATH="$OPTARG" ;;
-            v) BUILD_VERSION="$OPTARG" ;;
+            b) BUILD_COUNT="$OPTARG" ;;
+            v) CUSTOM_APP_VERSION="$OPTARG" ;;
             c) CHANGELOG="$OPTARG" ;;
             *) echo "Error: Invalid option"; exit 1 ;;
         esac
     done
 
     # Check if all mandatory options are provided
-    if [[ -z "$ENVIRONMENT" || -z "$APP_PATH" || -z "$BUILD_VERSION" ]]; then
+    if [[ -z "$ENVIRONMENT" || -z "$APP_PATH" || -z "$BUILD_COUNT" ]]; then
         echo "Error: Missing mandatory options."
-        echo "Usage: app-deploy tagging -e \"environment_name\" -p \"path/to/app.{ipa/apk}\" -v \"{build number}\""
-        echo "Example: app-deploy tagging -e \"internal-staging\" -p \"path/to/app.ipa\" -v \"42\""
+        echo "Usage: app-deploy tagging -e \"environment_name\" -p \"path/to/app.{ipa/apk}\" -b \"{build count}\""
+        echo "Example: app-deploy tagging -e \"internal-staging\" -p \"path/to/app.ipa\" -b \"42\""
         exit 1
     fi
 }
